6f1d025ce4f97fe36290a89b67e3a4d4cbc3d280
Shayne Geilman <shaynegeilman@gmail.com>
2023-04-04 13:59:48 +0800

    deleted base css, added description and header

---
+++ src/components/description.tsx
@@ -0,0 +1,22 @@
+function Description() {
+  return (
+    <div>
+      <div>
+        <h2>aboutMe()</h2>
+        <p>Placeholder text. </p>
+      </div>
+      <div>
+        <div>
+          <h3>Front-end Developer</h3>
+          <a>projects</a>
+          <span>&lt;/&gt;</span>
+        </div>
+        <div>
+          <h3>Freelancer</h3>
+          <a>Hire me!</a>
+          <span>{}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <div>
+      <h1>Shayne Geilman</h1>
+      <nav id="navbar">
+        <a>About</a>
+        <a>Skills</a>
+        <a>Projects</a>
+      </nav>
+      <div id="socials">
+        <a>LinkedIn</a>
+        <a>Github</a>
+        <a>Contact Me</a>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
