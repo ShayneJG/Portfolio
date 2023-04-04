7666de1eba75449e0c71521da3698f6339b1a0dc
Shayne Geilman <shaynegeilman@gmail.com>
2023-04-04 21:54:45 +0800

    skills updated.

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -4,9 +4,11 @@ function Skills() {
       <h2>skills()</h2>
       <div>
         <div id="left">
-          <ul>
-            {skillArray.map((item: skills) => {
-              return <ListCreator image={item.image} text={item.text} />;
+          <ul className="w-full flex">
+            {skillArray.map((item: skills, index) => {
+              return (
+                <ListCreator image={item.image} text={item.text} key={index} />
+              );
             })}
           </ul>
         </div>
@@ -23,13 +25,20 @@ type skills = {
 
 const skillArray: skills[] = [
   { image: "src/assets/javascript logo.png", text: "Javascript" },
+  { image: "src/assets/typescript-original 2.png", text: "Typescript" },
+  { image: "src/assets/HTML5_Badge_64.png", text: "HTML" },
+  { image: "src/assets/css logo.png", text: "CSS" },
+  { image: "src/assets/react logo.png", text: "React" },
+  { image: "src/assets/mongo logo.png", text: "MongoDB" },
+  { image: "src/assets/node logo.png", text: "NodeJS" },
+  { image: "src/assets/python-original 1.png", text: "Python" },
 ];
 
 function ListCreator(item: skills) {
   return (
-    <li key={item.text}>
-      <img src={item.image}></img>
-      <p>{item.text}</p>
+    <li className="m-auto" key={item.text}>
+      <img className="h-10 m-auto" src={item.image}></img>
+      <p className="">{item.text}</p>
     </li>
   );
 }
