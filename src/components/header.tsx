ee46c8cca436633cd7eb4b67c6a3780910ef73a2
Shayne Geilman <shaynegeilman@gmail.com>
2023-04-05 16:13:26 +0800

    font added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,19 @@
 function Header() {
   return (
-    <div>
-      <h1>Shayne Geilman</h1>
-      <nav id="navbar">
+    <div className="flex items-center flex-col">
+      <div className="flex">
+        <h1 className="text-lg text-[#828282] font-bold">SHAYNE </h1>
+        <h1 className="text-lg text-[#F2F2F2] font-bold">GEILMAN</h1>
+      </div>
+      <nav
+        id="navbar"
+        className="flex w-full px-10 justify-between text-white font-bold "
+      >
         <a>About</a>
         <a>Skills</a>
         <a>Projects</a>
       </nav>
-      <div id="socials">
+      <div id="socials" className="flex w-full px-10 justify-between ">
         <a>LinkedIn</a>
         <a>Github</a>
         <a>Contact Me</a>
