2a34f33865012f9c270434891e6fb1d832a16509
Shayne Geilman <shaynegeilman@gmail.com>
2023-04-10 00:49:41 +0800

    added contact form

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 function Header() {
+  const [contact, setContact] = useState<boolean>(false);
   return (
     <div className="flex items-center flex-col">
       <div className="flex">
@@ -17,7 +20,11 @@ function Header() {
         id="socials"
         className="flex w-full px-5 mt-3 justify-between text-white text-lg font-bold flex-wrap  items-center "
       >
-        <a className="flex items-center">
+        <a
+          className="flex items-center"
+          href="https://www.linkedin.com/in/shayne-geilman-b0731a96/"
+          target="_blank"
+        >
           <img
             className="h-4 pr-1"
             src="src/assets/linkedIn.png"
@@ -25,7 +32,11 @@ function Header() {
           />
           LinkedIn
         </a>
-        <a className="flex items-center">
+        <a
+          className="flex items-center"
+          href="https://github.com/ShayneJG"
+          target="_blank"
+        >
           <img
             className="h-4 pr-1"
             src="src/assets/github.png"
@@ -34,7 +45,12 @@ function Header() {
           Github
         </a>
         <div className="bg-gradient-to-t from-white to-[#70FF008C] rounded-md p-1">
-          <a className="flex items-center bg-black px-2 rounded-md">
+          <a
+            onClick={() => {
+              setContact(true);
+            }}
+            className="flex items-center bg-black px-2 rounded-md"
+          >
             <img
               className="h-3 pr-1"
               src="src/assets/contact.png"
@@ -43,6 +59,60 @@ function Header() {
             Contact me
           </a>
         </div>
+        {contact && (
+          <form
+            action="https://formspree.io/f/mrgvlapv"
+            method="POST"
+            id="contact"
+            className="absolut flex flex-col border-2 rounded-lg bg-black border-[#70FF00] left-0 right-0 bottom-0 mx-5 p-5 top-20 h-1/2 justify-between"
+          >
+            <div>
+              <label className="block mb-2">
+                Name
+                <input name="name" className="w-full text-black" type="text" />
+              </label>
+            </div>
+            <div>
+              <label className="block">
+                Email
+                <input
+                  name="email"
+                  className="w-full text-black"
+                  type="email"
+                />
+              </label>
+            </div>
+            <div>
+              <label className="block">
+                Contact number (optional)
+                <input
+                  name="number"
+                  className="w-full text-black"
+                  type="number"
+                />
+              </label>
+            </div>
+            <div>
+              <label className="block">
+                Comments
+                <textarea
+                  name="comments"
+                  className="w-full text-black"
+                ></textarea>
+              </label>
+            </div>
+            <div className="flex justify-between mx-20">
+              <button
+                onClick={() => {
+                  setContact(false);
+                }}
+              >
+                Cancel
+              </button>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        )}
       </div>
     </div>
   );
