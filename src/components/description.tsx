6220d7fefdedce4a811a02158b88ec6509c52844
Shayne Geilman <shaynegeilman@gmail.com>
2023-04-10 17:20:57 +0800

    Update description.tsx

--- src/components/description.tsx
+++ src/components/description.tsx
@@ -2,8 +2,26 @@ function Description() {
   return (
     <div>
       <div>
-        <h2 id="about">aboutMe()</h2>
-        <p>Placeholder text. </p>
+        <div className="font-bold flex mb-5">
+          <h2 id="about" className="text-[#70FF00] pl-10 pb-5 text-2xl  ">
+            aboutMe
+          </h2>
+          <p className="text-[#E0E0E0] text-2xl">( )</p>
+        </div>
+
+        <p className="text-[#F2F2F2] font-normal leading-8">
+          I am a self-taught developer eager to get started with a career in web
+          development. My coding journey began with Visual Basic in highschool
+          through to Java at TAFE. After a brief tangent in the publishing and
+          editing industry, I am eager to make a career shift into web
+          development. I started out by following the curriculum on
+          FreeCodeCamp, and after finishing their class-based React course, I
+          chose to independently teach myself the functional approach to React
+          to stay current. Since then, I have built out a 'manuscript
+          management' app that helps with my work in publishing and editing,
+          learnt Typescript, honed my skills with css frameworks such as
+          Tailwind, and learnt to apply professional designs using Figma.
+        </p>
       </div>
       <div>
         <div>
